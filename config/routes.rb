86fd85d09aadbc5539493f7693b8c0959c4bb57a
Michael Ogala <michael.ogala@andela.com>
2016-09-09 11:20:19 +0100

    Add devise gem for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get ':slug' => 'links#show'
 
   get 'links/destroy'
-
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
