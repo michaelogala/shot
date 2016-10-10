2fc12e3253eb1dff05477653b43a39f64025483c
Michael Ogala <michael.ogala@andela.com>
2016-10-10 18:17:07 +0100

    Add password validation and password confirmation validation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     first_name Faker::Name.first_name
     last_name Faker::Name.last_name
     email Faker::Internet.email
-    password Faker::Internet.email
+    password 'passworded'
+    password_confirmation 'passworded'
   end
 end
