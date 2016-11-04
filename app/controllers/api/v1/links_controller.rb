11aa1c3bb8957b52f5bcc429ff621dd635b6e84d
Michael Ogala <michael.ogala@andela.com>
2016-11-04 17:13:27 +0100

    create a link controller that responds to api calls

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -2,9 +2,51 @@ module Api
   module V1
     class LinksController < ApplicationController
       respond_to :json
+      before_action :find_link, only: [:show, :update, :destroy]
 
       def index
         render json: { links: Link.all }, status: 200
+      end
+
+      def show
+        render json: @link
+      end
+
+      def create
+        link = Link.new(link_params)
+        if link.save
+          render json: link, status: 200
+        else
+          render json: { error: new_link_error }
+        end
+      end
+
+      def update
+        if @link.update_attributes(link_params)
+          render json: link, status: 200
+        else
+          render json: { errors: @link.errors }, status: 200
+        end
+      end
+
+      def destroy
+        @link.destroy
+        head 204
+      end
+
+      private
+
+      def link_params
+        params.permit(
+                      :given_url,
+                      :slug,
+                      :active,
+                      :id
+                      )
+      end
+
+      def find_link
+        @link = Link.find_by(id: params[:id])
       end
     end
   end
--- spec/requests/api/v1/api_v1_links_spec.rb
+++ spec/requests/api/v1/api_v1_links_spec.rb
@@ -1,10 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Links", type: :request do
+  let(:link) { create :link }
+
   describe "GET /api_v1_links" do
-    it "works! (now write some real specs)" do
-      get api_v1_links_index_path
+    before { get '/api/v1/links' }
+
+    it 'returns all links' do
+      expect(json_response[:links]).to be_present
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /api/v1/links/:id' do
+    before { get "/api/v1/links/#{link.id}"}
+
+    it 'returns json response containing link properties' do
+      expect(json_response[:url]).to be_present
+      expect(json_response[:slug]).to be_present
+      expect(json_response[:active]).to be_present
+    end
+  end
+
+  describe 'POST /api/v1/links' do
+    let!(:link_count) { Link.count }
+
+    context 'with valid params' do
+      before { post '/api/v1/links', attributes_for(:link) }
+
+      it 'saves and returns the link as json response' do
+        expect(Link.count).to eq link_count + 1
+        expect(json_response[:url]).to be_present
+        expect(json_response[:slug]).to be_present
+      end
+    end
+
+    context 'with invalid params' do
+      before { post '/api/v1/links' }
+
+      it 'returns a json message indicating failure' do
+        expect(Link.count).to eq link_count
+        expect(json_response[:error]).to be_present
+      end
+    end
+  end
+
+  describe 'PATCH /api/v1/links/:id' do
+    context 'with valid params' do
+      before { patch "/api/v1/links/#{link.id}", { slug: 'newslug' } }
+
+      it 'updates the link data' do
+        expect(json_response[:slug]).to eq 'newslug'
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/links/:id' do
+    before { delete "/api/v1/links/#{link.id}" }
+
+    it 'removes the link from the database' do
+      expect(Link.find_by(id: link.id)).to be_nil
+      expect(response.status).to eq 204
     end
   end
 end
